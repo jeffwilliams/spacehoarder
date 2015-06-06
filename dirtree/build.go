098f6b7891d1e9244cd3e6a86d95ff05d3bc18fb
Jeff Williams <jeffsmessages@gmail.com>
2015-06-05 23:28:31 -0400

    Updated documentation

--- dirtree/build.go
+++ dirtree/build.go
@@ -8,41 +8,60 @@ import (
 	"time"
 )
 
+// Op is a type of operation on a Dirtree.
 type Op int
 
 const (
+	// Root sets the root of the tree.
 	Root Op = iota
+	// Add adds a node to a Dirtree.
 	Add
+	// Del deletes a node from a Dirtree.
 	Del
+	// Update modifies the contents of a node in a Dirtree.
 	Update
 )
 
+// OpData is an operation on a DirTree and it's corresponding data.
 type OpData struct {
 	Op   Op
 	Node *Node
 	Size int64
 }
 
+// Filesystem is an abstraction of a filesystem used by BuildFs.
 type Filesystem interface {
-	Open(name string) (file File, err error)
+	// Open opens a file with the specified path. If an error occurs opening the file
+	// then err is non-nil on return.
+	Open(path string) (file File, err error)
 }
 
+// File is an abstraction of a file.
 type File interface {
 	io.Closer
 	Readdir(count int) ([]os.FileInfo, error)
 }
 
+// OsFilesystem is a Filesystem that performs as expected; that is,
+// it opens files from the local filesystem.
 type OsFilesystem struct{}
 
-func (r OsFilesystem) Open(name string) (file File, err error) {
-	return os.Open(name)
+// Open opens the file with the specified path.
+func (r OsFilesystem) Open(path string) (file File, err error) {
+	return os.Open(path)
 }
 
-// Build a new Dirtree, and pass along all the operations made along the way.
+// Build builds a new Dirtree starting from the specified directory `basepath` and writes all
+// the operations performed to the Dirtree to the ops channel so that a copy of the Dirtree can be
+// made in a different goroutine. The paths processed are written to the channel prog.
 func Build(basepath string) (ops chan OpData, prog chan string) {
 	return BuildFs(OsFilesystem{}, basepath)
 }
 
+// BuildFs builds a new Dirtree starting from the specified directory `basepath` and writes all
+// the operations performed to the Dirtree to the ops channel so that a copy of the Dirtree can be
+// made in a different goroutine. The paths processed are written to the channel prog.
+// The Filesystem fs is used for opening files.
 func BuildFs(fs Filesystem, basepath string) (ops chan OpData, prog chan string) {
 
 	ops = make(chan OpData)
@@ -53,6 +72,8 @@ func BuildFs(fs Filesystem, basepath string) (ops chan OpData, prog chan string)
 	return
 }
 
+// BuildSync builds a new Dirtree starting from the specified directory `basepath` and returns it when
+// it's complete.
 func BuildSync(basepath string) *Dirtree {
 	return buildFs(OsFilesystem{}, basepath, nil, nil)
 }
@@ -152,6 +173,9 @@ func buildFs(fs Filesystem, basepath string, ops chan OpData, prog chan string) 
 	return tree
 }
 
+// Apply applies the operation `op` to the Dirtree `t`.
+// This is meant to be used to apply the operations output by
+// Build and BuildFs for creating a duplicate tree.
 func Apply(t *Dirtree, op OpData) {
 	switch op.Op {
 	case Root:
